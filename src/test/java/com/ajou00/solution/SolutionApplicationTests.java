3fabde4823aeb4d1801920cac4a22e24a823c30d
suminnnnn <tlsssm1212@ajou.ac.kr>
2023-08-29 17:04:57 +0900

    Initial commit

---
+++ src/test/java/com/ajou00/solution/SolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajou00.solution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
